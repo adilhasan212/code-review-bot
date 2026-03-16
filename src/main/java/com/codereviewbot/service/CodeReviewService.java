69af839bc058b645a0bc4e16aa7d23a701eaee96
Adil Hasan <adilhasan1233@gmail.com>
2026-03-16 00:31:08 -0500

    Better issueSummary formatting

--- src/main/java/com/codereviewbot/service/CodeReviewService.java
+++ src/main/java/com/codereviewbot/service/CodeReviewService.java
@@ -89,7 +89,7 @@ public class CodeReviewService {
             repoName,
             prNumber,
             issuesSize,
-            String.join(", ", issues),
+            String.join(",\n ", issues),
             LocalDateTime.now()
         );
 
